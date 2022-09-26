5cafa2bf25dc63fb65253768dee242068351021f
JotaPrimo <gestald118@gmail.com>
2022-09-26 18:48:29 -0400

    criando serviço de dados

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { DashboardModule } from "./dashboard/dashboard.module";
+import { DadosService } from "./dashboard/dados.service";
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DashboardModule
+  ],
+  providers: [
+    DadosService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/dashboard/intex.ts
@@ -0,0 +1,6 @@
+export * from './dashboard.module';
+export * from './dashboard.component';
+export * from './dados.service';
+
+
+
